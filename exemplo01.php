8317e25fa2f4752062aafeaf81b4b559943ed34d
JoaoPaulo2107 <161509047+JoaoPaulo2107@users.noreply.github.com>
2025-02-14 16:54:37 -0300

    segunda aula

---
+++ exemplo01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+
+    
+</head>
+<body>
+    <p>Oi sou o João</p>
+    <?php
+    echo "<p>Oi sou o João</p>";
+
+    $nome="joão";
+    echo "<p>Oi sou o $nome </p>";
+
+    echo "<p>Oi sou o ". $nome."</p>";
+    ?>
+
+
+</body>
+</html>
